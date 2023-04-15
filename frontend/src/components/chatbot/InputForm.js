2c8d500bc855eb63b9ae29d69a2949a7c1226c9a
ak2246@cornell.edu <50778020+Aarav-Khanna@users.noreply.github.com>
2023-04-15 18:24:56 -0400

    Connect Backend with Frontend

--- frontend/src/components/chatbot/InputForm.js
+++ frontend/src/components/chatbot/InputForm.js
@@ -24,32 +24,40 @@ const InputForm = (props) => {
 
   const checkForEthereum = (input) => {
     const triggerWord = 'ethereum';
-    return input.trim().toLowerCase() === triggerWord;
+    return input.trim().toLowerCase() === triggerWord.toLowerCase();
   };
-
+  
   const checkForSolana = (input) => {
     const triggerWord = 'solana';
-    return input.trim().toLowerCase() === triggerWord;
+    return input.trim().toLowerCase() === triggerWord.toLowerCase();
   };
 
   const checkForAvalanche = (input) => {
     const triggerWord = 'avalanche';
-    return input.trim().toLowerCase() === triggerWord;
-  };
+    return input.trim().toLowerCase() === triggerWord.toLowerCase();
+  }
 
   const checkForArbitrum = (input) => {
     const triggerWord = 'arbitrum';
-    return input.trim().toLowerCase() === triggerWord;
-  };
+    return input.trim().toLowerCase() === triggerWord.toLowerCase();
+  }
 
   const connectMetaMask = async () => {
     if (provider) {
       try {
-        const accounts = await provider.request({ method: 'eth_requestAccounts' });
+        const chainId = '0x' + parseInt(5).toString(16); // Avalanche Testnet Chain ID
+        await provider.request({
+          method: 'wallet_switchEthereumChain',
+          params: [{ chainId }],
+        });
+  
+        // You can also connect to the account if needed, similar to connectMetaMask
+        const accounts = await provider.request({ method: 'eth_requestAccounts' });   
         const account = accounts[0];
         console.log("Connected account:", account);
         sendToBackend(account);
-      } catch (err) {
+        props.onSendMessage(`Your Ethereum wallet has been connected! Your address in use is ${account}`, 'bot');
+    } catch (err) {
         if (err.code === 4001) {
           console.log('User rejected the request.');
         } else {
@@ -67,54 +75,93 @@ const InputForm = (props) => {
       alert('Please install the Phantom wallet to use this feature.');
       return;
     }
-
+  
     // Connect to the Phantom wallet
     const provider = window.solana;
     await provider.connect();
-
+  
     // Get the user's public key
     const publicKey = provider.publicKey.toBase58();
-
+  
     // Log the user's public key to the console
     console.log(`Connected to Phantom wallet with public key: ${publicKey}`);
     sendToBackend(publicKey);
   }
 
-  async function connectAvalancheCChain() {
-    if (window.ethereum && window.ethereum.isMetaMask) {
+  const connectAvalanche = async () => {
+    if (provider) {
       try {
-        const chainParams = {
-          chainId: '0xA869', // Avalanche C-Chain Mainnet Chain ID
-          chainName: 'Avalanche C-Chain TestNet (Fuji)',
-          nativeCurrency: {
-            name: 'AVAX',
-            symbol: 'AVAX',
-            decimals: 18,
-          },
-          rpcUrls: ['https://ava-testnet.public.blastapi.io/ext/bc/C/rpc'],
-          blockExplorerUrls: ['https://testnet.snowtrace.io/'],
-        };
-        await window.ethereum.request({ method: 'wallet_addEthereumChain', params: [chainParams] });
-        console.log('Avalanche C-Chain connected.');
-
-        // Request the connected account
-        const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-        const publicKey = accounts[0];
-        console.log('Connected account:', publicKey);
-
-        // Send the public key to the backend
-        sendToBackend(publicKey);
+        const chainId = '0x' + parseInt(43113).toString(16); // Avalanche Testnet Chain ID
+        await provider.request({
+          method: 'wallet_switchEthereumChain',
+          params: [{ chainId }],
+        });
+  
+        // You can also connect to the account if needed, similar to connectMetaMask
+        const accounts = await provider.request({ method: 'eth_requestAccounts' });
+        const account = accounts[0];
+        console.log("Connected account:", account);
+        sendToBackend(account);
+        props.onSendMessage(`Your Avalanche wallet has been connected! Your address in use is ${account}`, 'bot');
       } catch (error) {
-        console.error('Error connecting to Avalanche C-Chain:', error);
+        console.error(error);
+        if (error.code === 4902) {
+          console.log('The requested chainId cannot be added.');
+        } else {
+          console.log('There was an issue switching to the Avalanche network.');
+        }
       }
     } else {
       console.log('MetaMask is not installed.');
     }
-  }
+  };
+  
+  const connectArbitrum = async () => {
+    if (provider) {
+      try {
+        const chainId = '0x' + parseInt(421613).toString(16); // Avalanche Testnet Chain ID
+        await provider.request({
+          method: 'wallet_switchEthereumChain',
+          params: [{ chainId }],
+        });
+  
+        // You can also connect to the account if needed, similar to connectMetaMask
+        const accounts = await provider.request({ method: 'eth_requestAccounts' });
+        const account = accounts[0];
+        console.log("Connected account:", account);
+        sendToBackend(account);
+        props.onSendMessage(`Your Avalanche wallet has been connected! Your address in use is ${account}`, 'bot');
+      } catch (error) {
+        console.error(error);
+        if (error.code === 4902) {
+          console.log('The requested chainId cannot be added.');
+        } else {
+          console.log('There was an issue switching to the Avalanche network.');
+        }
+      }
+    } else {
+      console.log('MetaMask is not installed.');
+    }
+  };
 
+//   const sendToBackend = async (message) => {
+//     try {
+//       const response = await fetch('http://localhost:5000/api/messages', {
+//         method: 'POST',
+//         headers: {
+//           'Content-Type': 'application/json',
+//         },
+//         body: JSON.stringify({ message }),
+//       });
 
+//       const data = await response.json();
+//       console.log('Response from backend:', data);
+//     } catch (error) {
+//       console.error('Error sending message to backend:', error);
+//     }
+//   };
 
-  const sendToBackend = async (message) => {
+const sendToBackend = async (message) => {
     try {
       const response = await fetch('http://localhost:5001/api/messages', {
         method: 'POST',
@@ -134,32 +181,32 @@ const InputForm = (props) => {
   const handleSubmit = (event) => {
     event.preventDefault();
     if (checkForEthereum(input)) {
-      props.onSendMessage(input);
+      props.onSendMessage(input, 'user');
       connectMetaMask();
       sendToBackend(input);
       setInput('');
     } else if (checkForSolana(input)) {
-      props.onSendMessage(input);
+      props.onSendMessage(input, 'user');
       connectPhantom();
       sendToBackend(input);
       setInput('');
     } else if (checkForAvalanche(input)) {
-      props.onSendMessage(input);
-      connectAvalancheCChain();
-      sendToBackend(input);
-      setInput('');
-    }
-    else if (checkForArbitrum(input)) {
-      props.onSendMessage(input);
-      connectMetaMask();
-      sendToBackend(input);
-      setInput('');
-    } else {
-      props.onSendMessage(input);
+        props.onSendMessage(input, 'user');
+        connectAvalanche();
+        sendToBackend(input);
+        setInput('');
+    }else if (checkForArbitrum(input)) {
+        props.onSendMessage(input, 'user');
+        connectArbitrum();
+        sendToBackend(input);
+        setInput('');
+    }else {
+      props.onSendMessage(input, 'user');
       sendToBackend(input);
       setInput('');
     }
   };
+  
 
   return (
     <form className="input-form" onSubmit={handleSubmit}>
@@ -176,4 +223,3 @@ const InputForm = (props) => {
 };
 
 export default InputForm;
-
--- frontend/src/components/chatbot/MessageList.js
+++ frontend/src/components/chatbot/MessageList.js
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import './MessageList.css';
 
-const MessageList = ({ messages }) => {
+const MessageList = (props) => {
+  const messagesEndRef = useRef(null);
+
+  const scrollToBottom = () => {
+    messagesEndRef.current.scrollIntoView({ behavior: 'smooth' });
+  };
+
+  useEffect(scrollToBottom, [props.messages]);
+
   return (
     <div className="message-list">
-      {messages.map((message, index) => (
-        <div key={index} className={`message ${message.sender}`}>
+      {props.messages.map((message, index) => (
+        <div
+          key={index}
+          className={`message ${message.sender === 'user' ? 'user' : 'bot'}`}
+        >
           {message.text}
         </div>
       ))}
+      <div ref={messagesEndRef} />
     </div>
   );
 };
