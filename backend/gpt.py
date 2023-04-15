373d6d61884c847db113e6224aa3300e78c30a7e
ak2246@cornell.edu <50778020+Aarav-Khanna@users.noreply.github.com>
2023-04-15 18:35:04 -0400

    Trying to fix merge conflicts

--- backend/gpt.py
+++ backend/gpt.py
@@ -1,9 +1,6 @@
 import openai
 import re
-<<<<<<< HEAD
 from main import TX, send_tx
-=======
->>>>>>> happy-branch
 from dotenv import dotenv_values
 
 config = dotenv_values("../.env")
@@ -24,38 +21,6 @@ def extract_info(prompt):
 
     return response.choices[0].text.strip()
 
-<<<<<<< HEAD
-user_command = "I wanna send 0.00001 ETH from 0x9262c5fC4fFa3C756F050cb405c3c18B7aF49CF9 to 0x7f610402ccc4CC1BEbcE9699819200f5f28ED6e3"
-
-# Use ChatGPT API to extract key information
-extracted_info = extract_info(user_command)
-
-# Extract Ethereum addresses and amount using regex
-address_pattern = re.compile(r'(0x[0-9a-fA-F]{40})')
-addresses = [match.group(1) for match in address_pattern.finditer(extracted_info)]
-
-if len(addresses) >= 2:
-    receiver_address = addresses[1]
-    sender_address = addresses[0]
-else:
-    print("Failed to extract required addresses.")
-    receiver_address = None
-    sender_address = None
-
-amount_regex = re.search(r'(\d+(?:\.\d+)?)\s*ETH', extracted_info)
-
-if receiver_address and sender_address and amount_regex:
-    amount = float(amount_regex.group(1))
-    print(f"Receiver address: {receiver_address}")
-    print(f"Sender address: {sender_address}")
-    print(f"Amount: {amount} ETH")
-    
-    tx = TX(sender_address, receiver_address, amount)
-    send_tx(tx)
-
-else:
-    print("Failed to extract required information.")
-=======
 def process_user_command(user_command):
     # Declare local variables for sender and receiver addresses
     sender_address = None
@@ -121,4 +86,3 @@ def process_user_command(user_command):
     #print('/n')
 
     return {'sender_address': sender_address, 'receiver_address': receiver_address, 'amount': amount}
->>>>>>> happy-branch
--- backend/main.py
+++ backend/main.py
@@ -3,16 +3,6 @@ from dotenv import dotenv_values
 
 config = dotenv_values("../.env")
 
-<<<<<<< HEAD
-w3 = Web3(Web3.HTTPProvider(config["ALCHEMY_API_URL"]))
-
-class TX:
-
-    def __init__(self, frm, to, value):
-        self.frm = frm
-        self.to = to
-        self.value = value
-=======
 
 class TX:
 
@@ -32,24 +22,16 @@ class TX:
 
         self.frm = self.w3.to_checksum_address(frm)
         self.to = self.w3.to_checksum_address(to)
->>>>>>> happy-branch
 
     def to_dict(self):
         val = {
             "from": self.frm,
             'to': self.to,
-<<<<<<< HEAD
-            'value': w3.to_wei(self.value, "ether"),
-            "nonce": w3.eth.get_transaction_count(self.frm),
-            "gas": 21_000,
-            "gasPrice": w3.eth.gas_price
-=======
             'value': self.w3.to_wei(self.value, "ether"),
             "nonce": self.w3.eth.get_transaction_count(self.frm),
             "gas": 21_000,
             "gasPrice": self.w3.eth.gas_price,
             "chainId": self.chain_id
->>>>>>> happy-branch
         }
         return val
 
@@ -57,17 +39,8 @@ def send_tx(tx: TX):
 
     private_key = config["SAMPLE_PRIVATE_KEY"]
 
-<<<<<<< HEAD
-    signed_tx = w3.eth.account.sign_transaction(tx.to_dict(), private_key)
-
-    tx_hash = w3.eth.send_raw_transaction(signed_tx.rawTransaction)
-
-    print(w3.to_hex(tx_hash))
-
-=======
     signed_tx = tx.w3.eth.account.sign_transaction(tx.to_dict(), private_key)
 
     tx_hash = tx.w3.eth.send_raw_transaction(signed_tx.rawTransaction)
 
     print(tx.w3.to_hex(tx_hash))
->>>>>>> happy-branch
