65f3251d7a646312cb73333e146d61c030897a31
Kiernan Geoghegan <kpg184@gmail.com>
2023-04-14 22:44:01 -0400

    metamask functionality

--- frontend/src/components/chatbot/InputForm.js
+++ frontend/src/components/chatbot/InputForm.js
@@ -1,26 +1,73 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import detectEthereumProvider from '@metamask/detect-provider';
 import './InputForm.css';
 
-const InputForm = ({ onSendMessage }) => {
-  const [inputValue, setInputValue] = useState('');
+const InputForm = (props) => {
+  const [input, setInput] = useState('');
+  const [provider, setProvider] = useState(null);
+  
+  useEffect(() => {
+    const initProvider = async () => {
+      const detectedProvider = await detectEthereumProvider();
+      if (detectedProvider) {
+        setProvider(detectedProvider);
+      } else {
+        console.log('Please install MetaMask!');
+      }
+    };
+    initProvider();
+  }, []);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (inputValue.trim()) {
-      onSendMessage(inputValue);
-      setInputValue('');
+  const handleInputChange = (event) => {
+    setInput(event.target.value);
+  };
+
+  const checkForTriggerWord = (input) => {
+    const triggerWord = 'connect';
+    return input.trim().toLowerCase() === triggerWord;
+  };  
+
+  const connectMetaMask = async () => {
+    if (provider) {
+      try {
+        const accounts = await provider.request({ method: 'eth_requestAccounts' });
+        const account = accounts[0];
+        // You can now use the account for further actions
+        console.log("Connected account:", account);
+      } catch (err) {
+        if (err.code === 4001) {
+          console.log('User rejected the request.');
+        } else {
+          console.error(err);
+        }
+      }
+    } else {
+      console.log('MetaMask is not installed.');
+    }
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (checkForTriggerWord(input)) {
+      props.onSendMessage(input);
+      connectMetaMask();
+      setInput('');
+    } else {
+      props.onSendMessage(input);
+      setInput('');
     }
   };
 
   return (
     <form className="input-form" onSubmit={handleSubmit}>
       <input
+        className="input-field"
         type="text"
-        placeholder="Type your message here..."
-        value={inputValue}
-        onChange={(e) => setInputValue(e.target.value)}
+        value={input}
+        onChange={handleInputChange}
+        placeholder="Type your message..."
       />
-      <button type="submit">Send</button>
+      <button className="send-button" type="submit">Send</button>
     </form>
   );
 };
