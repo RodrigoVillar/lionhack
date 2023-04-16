766568a4fd2fb0e1887d29394f6ab2975316c58c
Kiernan Geoghegan <kpg184@gmail.com>
2023-04-16 02:42:56 -0400

    css + scroll fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,27 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import InputForm from './components/chatbot/InputForm.js';
 import './App.css';
 
 const App = () => {
   const [messages, setMessages] = useState([{ text: 'Hello! What network would you like to use today?', sender: 'bot' }]);
+  const messageListRef = useRef(null);
 
   const handleSendMessage = (messageText, sender) => {
     setMessages((prevMessages) => [...prevMessages, { text: messageText, sender }]);
   };
 
+  useEffect(() => {
+    if (messageListRef.current) {
+      messageListRef.current.scrollTop = messageListRef.current.scrollHeight;
+    }
+  }, [messages]);
+
   return (
     <div className="chatbot-container">
-      <div className="message-list">
+      <div className="header">
+        <h1>ChainChat</h1>
+      </div>
+      <div className="message-list" ref={messageListRef}>
         {messages.map((message, index) => (
           <div
             key={index}
